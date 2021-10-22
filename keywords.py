4217c01840ffddc9d1300075104d36127d900e17
Dawid Pastuszka <dawidpastuszka23x0@gmail.com>
2021-10-22 19:08:42 +0200

    Adds a file with keywords dict.

---
+++ keywords.py
@@ -0,0 +1,13 @@
+reserved = {
+	'if' : 'IF',
+	'else' : 'ELSE',
+	'for' : 'FOR',
+	'while' : 'WHILE',
+	'break' : 'BREAK',
+	'continue' : 'CONT',
+	'return' : 'RETURN',
+	'eye' : 'EYE',
+	'zeros' : 'ZEROS',
+	'ones' : 'ONES',
+	'print' : 'PRINT'
+}
