a0c725cb481bb76ff099872b50f2a30c2e9149df
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-11-04 17:50:07 +0100

    Adds summary.

---
+++ summaries/dawidpastuszka_patrykbusse_zadanie1.py
@@ -0,0 +1,96 @@
+import ply.lex as lex
+import sys
+
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONT',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
+
+tokens = ("MTX_SUM", "MTX_DIFFERENCE", "MTX_PRODUCT", "MTX_QUOTIENT", "ADD", "SUBTRACT", "MULTIPLY", "DIVIDE",
+          "SMALLER_OR_EQUAL", "LARGER_OR_EQUAL", "NOT_EQUAL", "EQUAL", "ID", "INT",
+          "FLOAT", "STRING") + tuple(reserved.values())
+
+literals = ['+', '-', '*', '/', '(', ')', '[', ']', '{', '}', ',', ';', ':', '\'', '=', '<', '>']
+
+t_MTX_SUM = r'\.\+'
+t_MTX_DIFFERENCE = r'\.\-'
+t_MTX_PRODUCT = r'\.\*'
+t_MTX_QUOTIENT = r'\./'
+t_ADD = r'\+='
+t_SUBTRACT = r'\-='
+t_MULTIPLY = r'\*='
+t_DIVIDE = r'/='
+t_SMALLER_OR_EQUAL = r'<='
+t_LARGER_OR_EQUAL = r'>='
+t_NOT_EQUAL = r'!='
+t_EQUAL = r'=='
+
+t_ignore = ' \t'
+
+
+def t_STRING(t):  # to correct
+    r'"(\\"|[^"])*"'
+    t.value = t.value[1:-1]
+    return t
+
+
+def t_FLOAT(t):  # modified
+    r'([1-9]\d*|0?)\.\d*(E-?\d+)?'
+    if t.value[0] == '.':
+        t.value = '0' + t.value
+    t.value = float(t.value)
+    return t
+
+
+def t_INT(t):
+    r'[1-9]\d*'
+    t.value = int(t.value)
+    return t
+
+
+def t_ID(t):
+    r'[_a-zA-Z][_\w]*'
+    t.type = reserved.get(t.value, "ID")
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_comment(t):
+    r'\#.*(\n|\Z)'
+    t.lexer.lineno += 1
+
+
+def t_error(t):
+    print("error in line:", t.lexer.lineno, " unknown expression:", t.value.split('\n', 1)[0].split(';', 1)[0])
+    t.lexer.skip(len(t.value.split('\n', 1)[0].split(';', 1)[0]))
+
+
+lexer = lex.lex()
+
+if __name__ == '__main__':
+
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "testInput.txt"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
+
+    text = file.read()
+    lexer.input(text)  # Give the lexer some input
+
+    for token in lexer:
+        print("(%d): %s(%s)" % (token.lineno, token.type, token.value))
