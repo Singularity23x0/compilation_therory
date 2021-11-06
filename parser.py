aa91beeef3f1436e10a2fc7b43f9c66359e9375a
MisticVoid <misticvoid@gmail.com>
2021-11-06 20:10:27 +0100

    new rules added and other corrected

--- parser.py
+++ parser.py
@@ -4,7 +4,11 @@ from lexer import *
 
 precedence = (
     ('nonasoc', 'IFX'),
-    ('nonasco', 'ELSE')
+    ('nonasco', 'ELSE'),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('left', 'MTX_SUM', 'MTX_DIFFERENCE'),
+    ('left', 'MTX_PRODUCT', 'MTX_QUOTIENT')
 )
 
 
@@ -15,29 +19,30 @@ def p_program(p):
 def p_segment(p):
     """segment
     : segment segment
-    | group"""
+    | group
+    | empty"""
 
 
 def p_line(p):
-    """line : expression ;"""
+    """line : expression ';'"""
 
 
 def p_block(p):
-    """block : { segment }"""
+    """block : '{' segment '}'"""
 
 
 def p_for(p):
-    """for : FOR ID = range group"""
+    """for : FOR ID '=' range group"""
 
 
 def p_while(p):
-    """while : WHILE ( logical_expression ) group"""
+    """while : WHILE '(' logical_expression ')' group"""
 
 
 def p_if(p):
     """if
-    : IF ( logical_expression ) group
-    | IF ( logical_expression ) group ELSE group %prec IFX"""
+    : IF '(' logical_expression ')' group
+    | IF '(' logical_expression ')' group ELSE group %prec IFX"""
 
 
 def p_group(p):
@@ -58,30 +63,105 @@ def p_expression(p):
     """expression
     : logical_expression
     | assignment
-    | arithmetic_expression"""
+    | arithmetic_expression
+    | matrix_expression
+    | string_expression"""
 
 
 def p_logical_expression(p):
     """logical_expression
     : arithmetic_expression comparison_operator arithmetic_expression
     | matrix_expression EQUAL matrix_expression
-    | matrix_expression NOT_EQUAL matrix_expression"""
+    | matrix_expression NOT_EQUAL matrix_expression
+    | string_expression comparison_operator string_expression"""
 
 
 def p_comparison_operator(p):
     """comparison_operator
-    : <
-    | >
+    : '<'
+    | '>'
     | EQUAL
     | NOT_EQUAL
     | SMALLER_OR_EQUAL
     | LARGER_OR_EQUAL"""
 
 
-def p_arithmetic_expression(p):
-    """arithmetic_expression
-    : INT
+def p_number(p):
+    """number
+    : - number
+    | INT
     | FLOAT
     | ID
     | arithmetic_expression """
+
+
+def p_arithmetic_expression(p):
+    """arithmetic_expression
+    : number
+    | number arithmetic_operator number"""
+
+
+def p_arithmetic_operator(p):
+    """arithmetic_operator
+    : +
+    | -
+    | *
+    | /"""
+
+
+def p_matrix(p):
+    """matrix
+    : - matrix
+    | matrix '\''
+    | ID
+    | matrix_function
+    | matrix_literal
+    | matrix_expression """
+
+
+def p_matrix_expression(p):
+    """matrix_expression
+    : matrix
+    | matrix matrix_operator matrix"""
+
+
+def p_matrix_operator(p):
+    """matrix_operator
+    : MTX_SUM
+    | MTX_DIFFERENCE
+    | MTX_PRODUCT
+    | MTX_QUOTIENT
+    | '+'
+    | '-'
+    | '*' """
+
+
+def p_string(p):
+    """string
+    : STRING
+    | ID
+    | string_expression """
+
+
+def p_string_expression(p):
+    """string_expression
+    : string
+    | string '+' string"""
+
+
+def p_range(p):
+    """range
+    : number ':' number"""
+
+
+def p_assignment(p):
+    """assignment
+    : ID '=' number
+    | ID '=' matrix
+    | ID '=' string
+    | ID '=' ID """
+
+
+
+
 
