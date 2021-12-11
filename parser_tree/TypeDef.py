8909c3e0bd9fb9bb9e8688cf9e178610bf6a4e5c
Singularity23x0 <dawidpastuszka23x0@gmail.com>
2021-12-11 15:11:40 +0100

    Adds main o TypeDef.

--- parser_tree/TypeDef.py
+++ parser_tree/TypeDef.py
@@ -7,15 +7,19 @@ class TypeDef:
         pass
 
     def get_type(self, operator, el1, el2=None):
-        if el2 is not None:
-            return get_type_binary(operator, el1, el2)
-        return get_type_unary(operator, el1)
+        if el2 is None:
+            return get_type_unary(operator, el1)
+        return get_type_binary(operator, el1, el2)
 
 
 def get_type_binary(operator, el1, el2):
     if not Types.equivalent(el1, el2):
         return None
+    if operator <= 4 and Types.is_matrix(el1):
+        return None
     if 5 <= operator <= 8 and not Types.is_matrix(el1):
+        return None
+    if operator >= 9:
         return None
     el1.core_type = max(el1.core_type, el2.core_type)
     return el1
@@ -27,3 +31,7 @@ def get_type_unary(operator, el):
     if operator == struct.Operator.UMINUS:
         return el
     return None
+
+
+if __name__ == '__main__':
+    pass
